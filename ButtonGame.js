a03e1c0bc76a2356f4d2a25082df7aeb2026db78
Ken <kjtchan@gmail.com>
2013-02-23 17:22:11 -0500

    Initial files

---
+++ ButtonGame.js
@@ -0,0 +1,23 @@
+$("#startgame").click(startClick);
+var ticker = 0;
+function startClick()
+{    
+    var timer = 60;
+    buttonMaker();
+}
+
+function buttonMaker()
+{
+    var left=Math.random()*85, top = Math.random()*85, size=Math.random()*15;
+    $("body").append("<button id="+ticker+">bbbbbbbbbb</button>");
+    $("#"+ticker).css("position", "absolute");
+    $("#"+ticker).css("left",left+"%");
+    $("#"+ticker).css("top",top+"%");
+    $("#"+ticker).css("height",size+"%");
+    $("#"+ticker).css("width", size+"%");
+    $("#"+ticker).css("width", size+"%");
+    //$("style").append("#"+ticker+"{position:absolute;left:"+left+"%;top:"+top+"%;width:"+size+"%;height:"+size+"%;}");
+    //var button = document.getElementbyID(ticker);
+   
+    ticker++;
+}    
